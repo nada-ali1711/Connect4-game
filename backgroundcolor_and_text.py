426dd1e0ab9d77f8f69a8f9fca6f79b8f2547080
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-11-10 19:52:06 +0200

    Create backgroundcolor_and_text.py

---
+++ backgroundcolor_and_text.py
@@ -0,0 +1,30 @@
+import pygame
+import sys
+
+
+pygame.init()
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+#define font
+font = pygame.font.SysFont('arial',75)
+pygame.display.set_caption("Connect4 Game")
+#define font color
+fontColor=(255,255,255)
+
+def draw_text(text,font, color,x,y):
+    show = font.render(text,True,color)
+    screen.blit(show,(x,y))
+
+
+
+run = True
+while run:
+    screen.fill((50,80,90))
+    for event in pygame.event.get():
+        draw_text("Connect4 Game",font,fontColor,160,200)
+        if event.type==pygame.QUIT:
+            run=False
+        pygame.display.update()
+pygame.quit()
+
